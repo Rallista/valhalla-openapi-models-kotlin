0d61bebbfd11fc3ed90c9162408fe0e8aa55188f
Jacob Fielding <jf0517@gmail.com>
2026-02-11 18:54:20 -0800

    feat: valhalla 3.6.2 compatible config

--- config/src/test/kotlin/ValhallaConfigTest.kt
+++ config/src/test/kotlin/ValhallaConfigTest.kt
@@ -1,22 +1,11 @@
-import com.squareup.moshi.FromJson
 import com.squareup.moshi.Moshi
-import com.squareup.moshi.ToJson
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import com.valhalla.config.ValhallaConfigBuilder
 import com.valhalla.config.models.ValhallaConfig
-import java.math.BigDecimal
 import kotlin.test.Test
 import kotlin.test.assertNotNull
 
 class ValhallaConfigTest {
-
-  object BigDecimalAdapter {
-    @FromJson
-    fun fromJson(value: String): BigDecimal = BigDecimal(value)
-
-    @ToJson
-    fun toJson(value: BigDecimal): String = value.toPlainString()
-  }
 
   private val moshi: Moshi = Moshi.Builder()
     .add(BigDecimalAdapter)
